7c301cd6768a3f2e0faa91f9b8895210218dffb3
Joseph Bernadas <jb3rns@gmail.com>
2020-06-14 00:07:53 -0700

    Tested printing individual tables

--- table-scraper.py
+++ table-scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import lxml
 import requests
 # import os
 
@@ -8,13 +9,22 @@ url =  "https://trip.llnl.gov/results/results2018-1.html"
 html_content = requests.get(url).text
   
 # Parses the HTML content
-soup = BeautifulSoup(html_content, "lxml")
+soup = BeautifulSoup(html_content, 'lxml')
 pageTitle = soup.h2
 pageSubTitle = soup.h4
 tables = soup.find_all('table', attrs={"border": "4"})
 
-print(pageTitle)
-print(pageSubTitle)
-print(tables)
-
-with open("")
+# print(pageTitle)
+# print(pageSubTitle)
+print("<!-------Table 1------->")
+print(tables[0])
+print("<!------Table 2-------->")
+print(tables[1])
+print("<!-------Table 3-------->")
+print(tables[2])
+print("<!-------Table 4-------->")
+print(tables[3])
+print("<!-------Table 5-------->")
+print(tables[4])
+print("<!-------Table 6-------->")
+print(tables[5])
