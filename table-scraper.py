59cc441d387971b7fbead361036a28d00749b98e
Joseph Bernadas <jb3rns@gmail.com>
2020-06-14 22:24:03 -0700

    Added code info and updated comments

--- table-scraper.py
+++ table-scraper.py
@@ -1,3 +1,9 @@
+# Author: Joseph Bernadas
+# Description: Converts old style tables to Bootstrap ready
+# Version: 1.0.0
+# License: GPL version 3
+# Date: 2020/06/14
+
 from bs4 import BeautifulSoup
 import lxml
 import requests
@@ -26,11 +32,11 @@ for link in initialSoup.find_all('a', attrs={'href': re.compile('^results')}):
     harvestedLinks.append(link.get('href'))
 
 ###########################################
-######### / End of Links Harvester ########
+##### / End of Link Crawler/Harvester #####
 ###########################################
 
 ###########################################
-######## Table Maker begins ###############
+######### Table Re begins ###########
 ###########################################
 
 # Stores incremented value
@@ -38,9 +44,9 @@ i = 0
 
 # Loops through each of the harvested links
 for harvestedLink in harvestedLinks:
-    # Used for the HTML header and body
+    # Used for the HTML header and body (Comment-out to output just the tables without HTML 5 header)
     htmlHead = """<!DOCTYPE html><html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <style>body{text-align: center}.link-wrapper{width: 100%; text-align: center}a.btn.btn-default{padding: 3px 20px; margin: 5px}.navbar-nav{float:none; margin: 0 auto; display: table; table-layout: fixed;}</style> </head> <body> <div class='container'><div class='row'><div class='col-12'>"""
-    # Closes the above HTML and body
+    # Closes the above HTML and body (Comment-out to output plain tables)
     htmlEnd = """</div></div></div></body></html>"""
     # The navigation bar
     anchorLinksNav = """<div class='link-wrapper'><ul class="nav navbar-nav"> <li><a class='btn btn-default' href='#i125'>I-125</a></li><li><a class='btn btn-default' href='#i131'>I-131</a></li></ul></div>"""
@@ -61,12 +67,12 @@ for harvestedLink in harvestedLinks:
     # Finds only the tables with a border attribute of 4
     tables = soup.find_all('table', attrs={"border": "4"})
 
-    # Tables only with anchor tags and links
+    # Used for tables only with anchor tags and nav-links
     tables.insert(0, anchorLinksNav)
     tables.insert(1, anchorI125)
     tables.insert(5, anchorI131)
 
-    # Used for displaying as HTML webpage
+    # Used for displaying as HTML webpage (Comment-out to output just the tables)
     tables.insert(0, htmlHead)
     tables.append(htmlEnd)
     tables.insert(1, pageTitle)
