36cc47c2d86e23c4baedea5c4e8efcd9ad788358
jbernadas <jb3rns@gmail.com>
2020-06-14 17:18:25 -0700

    Finally working!

--- table-scraper.py
+++ table-scraper.py
@@ -8,28 +8,28 @@ import os
 
 targetUrl = "https://trip.llnl.gov/results.html"
 
-harvestedLinks = []
+harvestedLinks = ["results/results2019-2.html"]
 
-html_page = requests.get(targetUrl).text
-soup = BeautifulSoup(html_page, "lxml")
-harvestTable = soup.find_all('table', attrs={'width': '714'})
+# html_page = requests.get(targetUrl).text
+# soup = BeautifulSoup(html_page, "lxml")
+# harvestTable = soup.find_all('table', attrs={'width': '714'})
 
-for link in soup.find_all('a', attrs={'href': re.compile('^results')}):
-    harvestedLinks.append(link.get('href'))
+# for link in soup.find_all('a', attrs={'href': re.compile('^results')}):
+#     harvestedLinks.append(link.get('href'))
 
 i = 0
 
 ######## Table Maker begins #######
 for harvestedLink in harvestedLinks:
 
-    htmlHead = """<!DOCTYPE html><html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <style>body{text-align: center}.link-wrapper{width: 100%; text-align: center}.navbar-nav{float:none; margin: 0 auto; display: table; table-layout: fixed;}</style> </head> <body> <div class='container'><div class='row'><div class='col-12'>"""
+    htmlHead = """<!DOCTYPE html><html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <style>body{text-align: center}.link-wrapper{width: 100%; text-align: center}a.btn.btn-default{padding: 3px 20px; margin: 5px}.navbar-nav{float:none; margin: 0 auto; display: table; table-layout: fixed;}</style> </head> <body> <div class='container'><div class='row'><div class='col-12'>"""
 
     htmlEnd = """</div></div></div></body></html>"""
 
-    anchorLinksNav = """<div class='link-wrapper'><ul class="nav navbar-nav"> <li><a href='#i125'>I-125</a></li><li><a href='#i131'>I-131</a></li></ul></div>"""
+    anchorLinksNav = """<div class='link-wrapper'><ul class="nav navbar-nav"> <li><a class='btn btn-default' href='#i125'>I-125</a></li><li><a class='btn btn-default' href='#i131'>I-131</a></li></ul></div>"""
 
-    anchorI125 = "<a id='i125'></a>"
-    anchorI131 = "<a id='i131'></a>"
+    anchorI125 = "<a id='i125'></a><p><h3>I-125 Results</h3></p>"
+    anchorI131 = "<a id='i131'></a><p><h3>I-131 Results</h3></p>"
 
     html_content = requests.get("https://trip.llnl.gov/" + harvestedLink).text
 
